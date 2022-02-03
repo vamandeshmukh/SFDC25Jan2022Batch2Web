ba0e631e335ba528ad75a2bd3897f97de5a7f073
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-02-03 15:40:26 +0530

    js scripts updated

--- js_demos/05-js-functions.js
+++ js_demos/05-js-functions.js
@@ -1,8 +1,16 @@
 // node 05-js-functions.js
 
+// old JS function 
 function fun() {
     console.log('fun');
 }
 
 fun();
+
+// modern JS - arrow function 
+const fun2 = () => {
+    console.log('fun2');
+}
+
+fun2();
 
