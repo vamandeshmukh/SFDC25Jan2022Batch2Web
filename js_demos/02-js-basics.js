38fb67b2de1ec697fc80278a53d8c140efea31e1
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-02-03 14:26:28 +0530

    commit

--- js_demos/02-js-basics.js
+++ js_demos/02-js-basics.js
@@ -18,13 +18,34 @@ comment
 
 console.log('Some text'); 
 
-let myName = 'Vaman';
-let mySalary = 10.5;
-let myBonus = 10;
-let isIndian = true; 
+// let str
+
+let myName = `Vaman`; // string 
+let mySalary = 10.5; //number 
+let myBonus = 10; // number 
+let isIndian = true; // boolean 
 
 console.log(myName);
 console.log(mySalary);
 console.log(mySalary + myBonus);
 console.log(isIndian);
+
+// int num2 = 10;
+
+
+let num = 10;
+
+// num2 = 20;  // don't use in modern JS 
+// var num3 = 30; // don't use in modern JS 
+// use let and const in modern JS 
+let num4 = 40; // value can change throughout the program execution 
+const num5 = 50; // value can not change 
+console.log(num4);
+console.log(num5);
+num4 = 45;
+num5 = 55;
+console.log(num4);
+console.log(num5);
+
+
 
