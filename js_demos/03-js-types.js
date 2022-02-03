7b2e13d3ff438eed363c75938ee9aeb731b28f46
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-02-03 14:41:22 +0530

    js scripts updated

--- js_demos/03-js-types.js
+++ js_demos/03-js-types.js
@@ -1,17 +1,27 @@
 
 // types and assignments 
 
-let num1 = 10;
-let num2 = 'India'; // "India"   'India'   `India`
-let num3 = true;
-let num4;
-console.log(num1);
-console.log(typeof(num1));
-console.log(num2);
-console.log(typeof(num2));
-console.log(num3);
-console.log(typeof(num3));
-console.log(num4);
-console.log(typeof(num4));
+// let num1 = 10;
+// let num2 = 'India'; // "India"   'India'   `India`
+// let num3 = true;
+// let num4;
+// console.log(num1);
+// console.log(typeof(num1));
+// console.log(num2);
+// console.log(typeof(num2));
+// console.log(num3);
+// console.log(typeof(num3));
+// console.log(num4);
+// console.log(typeof(num4));
 
+// a let variable can accept different values 
 
+let num5 = 10;
+console.log(num5);
+console.log(typeof (num5));
+num5 = 'Some String value';
+console.log(num5);
+console.log(typeof (num5));
+num5 = true;
+console.log(num5);
+console.log(typeof (num5));
