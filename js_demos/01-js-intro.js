c9c7b4aeeaeb7cac2c5a896da98828e9478a3f76
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-02-03 12:49:21 +0530

    commit

--- js_demos/01-js-intro.js
+++ js_demos/01-js-intro.js
@@ -1,8 +1,13 @@
-
+ 
 // JavaScript demo 
 
+// function definition  
 function fun() {
     console.log("fun function...");
+    console.log(10 + 20);
 }
 
+// function invocation / function call 
 fun();
+
+
---
+++ js_demos/02-js-basics.js
@@ -0,0 +1,30 @@
+// run  node 02-js-basics.js
+
+// JS basics 
+
+// datatypes in JS 
+// 1. primnitive types 
+// number, string, boolean 
+// 2. reference types 
+// objects 
+
+
+// single line comment 
+
+/*
+multi line 
+comment 
+*/
+
+console.log('Some text'); 
+
+let myName = 'Vaman';
+let mySalary = 10.5;
+let myBonus = 10;
+let isIndian = true; 
+
+console.log(myName);
+console.log(mySalary);
+console.log(mySalary + myBonus);
+console.log(isIndian);
+
