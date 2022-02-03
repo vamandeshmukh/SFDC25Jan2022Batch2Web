ca36da23bc56b57e8fdbaa8b5b973d0f88306a3a
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-02-03 15:19:38 +0530

    js scripts updated

--- js_demos/03-js-types.js
+++ js_demos/03-js-types.js
@@ -16,12 +16,53 @@
 
 // a let variable can accept different values 
 
-let num5 = 10;
-console.log(num5);
-console.log(typeof (num5));
-num5 = 'Some String value';
-console.log(num5);
-console.log(typeof (num5));
-num5 = true;
-console.log(num5);
-console.log(typeof (num5));
+// let num5 = 10;
+// console.log(num5);
+// console.log(typeof (num5));
+// num5 = 'Some String value';
+// console.log(num5);
+// console.log(typeof (num5));
+// num5 = true;
+// console.log(num5);
+// console.log(typeof (num5));
+
+// NaN 
+
+// let num6 = 10;
+// let num7;
+// console.log(num6 + num7);
+
+//
+
+// truthy and false values 
+// falsy values - false, 0, null, undefined, NaN  
+// truthy values - rest all 
+
+// let userName; // input from user  // abc a@b.c 
+
+// if (userName) {
+//         console.log('Do this');
+// }
+// else {
+//     console.log('Do that')
+// }
+
+//   == and === operators
+//   != and !== operators
+
+let num8 = '10';
+// if (num8 === 10) { // 'Do that'
+    if (num8 == 10) { // 'Do this'
+    console.log('Do this');
+}
+else {
+console.log('Do that')
+}
+
+// arithmetic operators 
+console.log(10 + 3);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 3);
+console.log(10 % 3);
+
---
+++ js_demos/04-js-arrays-objects.js
@@ -0,0 +1,41 @@
+// node 04-js-arrays-objects.js
+
+// arrays in JS 
+
+// let myArray = [10, 'Abc', true, null, 20.5];
+// let myMarks = [99, 95, 97, 96, 98];
+
+// console.log(myArray);
+// console.log(myMarks);
+// console.log(myMarks[2]);
+
+// for (marks of myMarks) {
+//     console.log(marks);
+// }
+
+// objects in JS 
+// 1. proper JavaScript objects 
+// 2. objects of class 
+
+// 1. proper JS objects 
+
+let empData = {
+    eid: 101,
+    firstName: 'Sonu',
+    salary: 10.5,
+    isMarried: false,
+    phones: [9876543210, 7894561230],
+    address: {
+        area: 'AS Rao Nagar',
+        city: 'Hyderabad',
+        pin: 500083
+    }
+};
+
+console.log(empData);
+console.log(empData.firstName);
+console.log(empData.address.city);
+
+
+
+
