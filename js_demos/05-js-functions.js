2f5a2292c192c33d44fa7b5b6063e362a18f2914
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-02-03 15:53:07 +0530

    js scripts updated

--- js_demos/05-js-functions.js
+++ js_demos/05-js-functions.js
@@ -1,16 +1,46 @@
 // node 05-js-functions.js
 
-// old JS function 
-function fun() {
-    console.log('fun');
+// // old JS function 
+// function fun() {
+//     console.log('fun');
+// }
+
+// fun();
+
+// // modern JS - arrow function 
+// const fun2 = () => {
+//     console.log('fun2');
+// }
+
+// fun2();
+
+//// JS functions always return undefined, if they don't have return statement.
+
+// const fun4 = () => {
+//     console.log(' no args no return value');
+// }
+// console.log(fun4()); // undefined 
+
+
+// // functions with return value
+
+// const valueOfPi = () => {
+//     return 3.142;
+// }
+// const num = valueOfPi();
+// console.log(num);
+
+// functions with arguments 
+
+const fun3 = (a, b) => {
+    return a + b;
 }
 
-fun();
-
-// modern JS - arrow function 
-const fun2 = () => {
-    console.log('fun2');
-}
-
-fun2();
+// const num2 = fun3(10, 20);
+// console.log(num2);
+// shortcut 
+console.log(fun3(10, 20)); // 20 
+console.log(fun3(10)); // NaN
+console.log(fun3(10, 20, 30)); // 30
+console.log(fun3()); // NaN
 
