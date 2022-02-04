d7dab07b97f5d7b62ba47455bf8aae9e4127e971
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-02-04 10:22:36 +0530

    js scripts updated

--- js_demos/05-js-functions.js
+++ js_demos/05-js-functions.js
@@ -71,21 +71,22 @@ const fun5 = (arg) => {
 }
 
 const funAsArg = () => {
-    console.log('funAsArg invoked.')
+    console.log('funAsArg invoked.');
 }
-
+// invoke a function - 1
 fun5(funAsArg); // function call 
+// invoke a function - 2
+fun5(() => {console.log('anonymous function invoked.');});
 
-
-
+// define a funciton 
 const addNums = (a, b) => {
 console.log(a + b);
 }
-
+// invoke a function - 1
 let x = 10;
-let y = 10;
-
+let y = 20;
 addNums(x, y);
-
+// invoke a function - 2
+addNums(10, 20);
 
 
---
+++ js_demos/06-js-callbacks.js
@@ -0,0 +1,26 @@
+// callbacks in JS
+// functions passed as arguments to another function  
+
+const fun5 = (arg) => {
+    console.log('fun5 invoked.');
+    arg(); // function call  
+}
+
+const funAsArg = () => {
+    console.log('funAsArg invoked.');
+}
+// invoke a function - 1
+fun5(funAsArg); // function call 
+// invoke a function - 2
+fun5(() => {console.log('anonymous function invoked.');});
+
+// define a funciton 
+const addNums = (a, b) => {
+console.log(a + b);
+}
+// invoke a function - 1
+let x = 10;
+let y = 20;
+addNums(x, y);
+// invoke a function - 2
+addNums(10, 20);
