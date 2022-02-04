593ba08f54c4a63e99b787a372475f4711e06e10
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-02-04 12:01:36 +0530

    js scripts updated

--- js_demos/08-js-async.js
+++ js_demos/08-js-async.js
@@ -11,11 +11,20 @@
 // setTimeout(arg1 is a function, arg2 is number of milliseconds);
 // setTimeout(() => {} , 2000);
 
-console.log("statement 1");
+console.log("statement 1"); // loads from HTML document  
+
 setTimeout(() => {
-    console.log("statement 2"); // takes 3 seconds to execute  
+    console.log("statement 2"); // loads from database   
 },
     2000);
-console.log("statement 3");
+
+console.log("statement 3"); // loads from twitter 
 
 // OP: 1 3 2 
+
+// assignment - do this 
+// setTimeout( (arg) => { some code , arg()} , 2000 );
+// let funAsArg =() => {}
+
+// OP: 1 2 3   
+
